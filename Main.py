40e4fd2a7564b606e306755700518e9d6333de53
Sebastian Sjöling Njord <sesj0103@storsjogymnasiet.se>
2023-03-14 10:43:16 +0100

    Laddade upp på github

--- Main.py
+++ Main.py
@@ -4,12 +4,31 @@ looping = True
 
 def print_fruit(userinput):
     fnr = int(userinput)
-    print("\n" + frukter[fnr-1] + "Kommer här!")
+    print("\n" + frukter[fnr-1] + "Kommer här! \n")
 
 
 while looping:
+    print("----------------------------------------------------")
+    print("-:Fruktautomat:- \n")
+
+    i=1
+
+    for frukt in frukter:
+        print(str(i) + ": " + frukt)
+        i+=1
+
+    fruktnr = input("\nMata i nr på frukt du vill ha: ")
+
+    print_fruit(fruktnr)
+
     go = input("Vill du välja en frukt till? J/N")
     print("\n")
 
     if (go == "n"):
         break
+
+
+print("---------------------------------------------------------")
+print("Du får en frukt till")
+slumpfrukt = random.randint(1, 4)
+print_fruit(slumpfrukt)
